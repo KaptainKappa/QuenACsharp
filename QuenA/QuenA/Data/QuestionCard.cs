e41c6f70809a81141c8cb9737cf6f9413cb57705
KaptainKappa <rj.edwards@effeasoftworks.co.uk>
2014-01-24 12:08:37 +0000

    added image attributes for both question and answer to QuestionCard class

--- QuenA/QuenA/Data/QuestionCard.cs
+++ QuenA/QuenA/Data/QuestionCard.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace QuenA.Data
 {
@@ -21,12 +22,6 @@ namespace QuenA.Data
         //An (invisible) RichTextBox for changing RTF to plain text (see: FlavourText property).
         private static System.Windows.Forms.RichTextBox rtBox = new System.Windows.Forms.RichTextBox();
 
-
-
-        //string that separates fields when card is outputted as a string
-        private static string fieldSeparator = "/|/|/| \n"; //must be a string that should never come up in normal usage of the program
-        public static string FIELD_SEPARATOR { get { return fieldSeparator; } }
-
         //Text to go in the Question box
         private string questionText;
         public string QuestionText
@@ -43,6 +38,22 @@ namespace QuenA.Data
             }
         }
 
+        //Image to be attached to question
+        private Image questionImage;
+        public Image QuestionImage
+        {
+            get
+            {
+                return questionImage;
+            }
+            set
+            {
+                //set text when viewing the question as a whole
+                questionImage = value;
+                updateEditDate();
+            }
+        }
+
         //Text to go in the Answer box
         private string answerText;
         public string AnswerText
@@ -54,6 +65,23 @@ namespace QuenA.Data
             set
             {
                 answerText = value;
+                updateEditDate();
+            }
+        }
+
+
+        //Image to be attached to answer
+        private Image answerImage;
+        public Image AnswerImage
+        {
+            get
+            {
+                return answerImage;
+            }
+            set
+            {
+                //set text when viewing the question as a whole
+                answerImage = value;
                 updateEditDate();
             }
         }
@@ -109,30 +137,22 @@ namespace QuenA.Data
         /// Construct a QuestionCard identical to the one passed as parameter.
         /// </summary>
         /// <param name="card"></param>
-        public QuestionCard(QuestionCard card) : this(card.questionText, card.answerText, card.acknowledgementsText){
-            
-        }
-
-        /// <summary>
-        /// Construct a question card without acknowledgements.
-        /// </summary>
-        /// <param name="question">Text to go in the Question box when this card is viewed.</param>
-        /// <param name="answer">Text to go in the Answer box when this card is viewed.</param>
-        public QuestionCard(string question, string answer)
+        public QuestionCard(QuestionCard card)
+            : this(card.questionText, card.answerText, card.acknowledgementsText, card.QuestionImage, card.AnswerImage)
         {
-            initialiseText(question, answer, null);
+            //deliberately left blank
         }
 
-
         /// <summary>
-        /// Construct a question card with acknowledgements.
+        /// Construct a question card with images.
         /// </summary>
         /// <param name="question">Text to go in the Question box when this card is viewed.</param>
         /// <param name="answer">Text to go in the Answer box when this card is viewed.</param>
         /// <param name="acknowledgements">Text to go in the Acknowledgements box when this card is viewed.</param>
-        public QuestionCard(string question, string answer, string acknowledgements)
+        public QuestionCard(string questionTxt, string answerTxt, string acknowledgements, Image questionImg, Image answerImg)
         {
-            initialiseText(question, answer, acknowledgements);
+            initialiseText(questionTxt, answerTxt, acknowledgements);
+            initialiseImages(questionImg, answerImg);
         }
 
         /// <summary>
@@ -151,45 +171,29 @@ namespace QuenA.Data
         }
 
         /// <summary>
+        /// Initialise all images of a question card, if any.
+        /// </summary>
+        /// <param name="qustionImg">Image to be attached to the question text.</param>
+        /// <param name="answerImg">Image to be attached to the answer text</param>
+        private void initialiseImages(Image questionImg, Image answerImg)
+        {
+            if (questionImg != null)
+            {
+                questionImage = questionImg;
+            }
+
+            if (answerImg != null)
+            {
+                answerImage = answerImg;
+            }
+        }
+
+        /// <summary>
         /// Updates the dateLastEdited field to the current date.
         /// </summary>
         private void updateEditDate()
         {
             dateLastEdited = DateTime.Today;
         }
-
-        /// <summary>
-        /// Generate and return a string representation of this object.
-        /// </summary>
-        /// <returns></returns>
-        public string toString()
-        {
-            string output = "";
-
-            //add date created to string
-            output += dateCreated.ToShortDateString() + "\n";
-            //separate date created and date last separated
-            output += FIELD_SEPARATOR;
-            //add date last edited to string
-            output += dateLastEdited.ToShortDateString() + "\n";
-            //separate date last edited and question text
-            output += FIELD_SEPARATOR;
-            //add question text
-            output += questionText + "\n";
-            //separate question text and answer text
-            output += FIELD_SEPARATOR;
-            //add answer text
-            output += answerText + "\n";
-            //separate answer text and acknowledgements 
-            output += FIELD_SEPARATOR;
-            //add acknowledgement text
-            output += acknowledgementsText + "\n";
-
-            //return completed string
-            return output;
-
-
-        }
-
     }
 }
